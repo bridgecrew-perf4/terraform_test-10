df834eedb311a556aee4ba178db5f376d3862b66
Tatsuya Baba <tatsuya_baba@so-netmedia.jp>
2021-04-01 15:34:38 +0900

    change

--- projects/base/main.tf
+++ projects/base/main.tf
@@ -14,3 +14,11 @@ module "security_group" {
 module "iam_role" {
     source = "../../modules/iam_role"
 }
+
+output "role_cloudwatch" {
+    value = module.iam_role.role_cloudwatch.name
+}
+
+output "sg_private" {
+    value = module.security_group.sg_private.id
+}
--- projects/base/output.tf
+++
@@ -1,7 +0,0 @@
-output "role_cloudwatch" {
-    value = module.aws_iam_role.role_cloudwatch.name
-}
-
-output "sg_private" {
-    value = module.aws_security_group.sg_private.id
-}

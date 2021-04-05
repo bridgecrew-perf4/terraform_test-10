3ec4efa12332572a2cba1fb2067712a8e8fcf2e9
Tatsuya Baba <tatsuya_baba@so-netmedia.jp>
2021-04-05 11:00:42 +0900

    change

--- projects/airflow/production/main.tf
+++ projects/airflow/production/main.tf
@@ -1,6 +1,7 @@
 module "instance" {
+    count = 2
     source = "../../../modules/instance"
-    name = "trader_airflow_production"
+    name = "trader_airflow_production_${count.index}"
     instance_type = "t3.micro"
     security_group_id = data.terraform_remote_state.base.outputs.sg_private
     iam_role = data.terraform_remote_state.base.outputs.role_cloudwatch

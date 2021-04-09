752b7135200d9938bb0d3a22eb1dd6e98f753ab3
Tatsuya Baba <tatsuya_baba@so-netmedia.jp>
2021-04-09 15:18:03 +0900

    change

--- iam.tf
+++ iam.tf
@@ -6,10 +6,18 @@ data "aws_iam_policy" "change_password_policy" {
   arn = "arn:aws:iam::aws:policy/IAMUserChangePassword"
 }
 
+data "template_file" "pass_role_policy_template" {
+  template = file("./iam_policies/pass_role_policy.json")
+}
+
+data "template_file" "force_mfa_policy_template" {
+  template = file("./iam_policies/force_mfa_policy.json")
+}
+
 module "pass_role_policy" {
   source = "./modules/iam/policy"
   name = "pass_role_policy"
-  policy = file("iam_policies/pass_role_policy.json")
+  policy = data.template_file.pass_role_policy_template.rendered
 }
 
 output "pass_role_policy" {
@@ -19,7 +27,7 @@ output "pass_role_policy" {
 module "force_mfa_policy" {
   source = "./modules/iam/policy"
   name = "force_mfa_policy"
-  policy = file("iam_policies/force_mfa_policy.json")
+  policy = data.template_file.force_mfa_policy_template.rendered
 }
 
 output "force_mfa_policy" {
